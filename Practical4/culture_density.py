87474b980aa124a7fb7ce4410cdf278bed1faae9
xiangjunle <junle.23@intl.zju.edu.cn>
2024-07-13 12:56:43 +0800

    again

---
+++ Practical4/culture_density.py
@@ -0,0 +1,14 @@
+# Start with the initial density
+initial_density = 5
+# Initialize the day counter
+day = 1
+# Initialize the current density
+current_density = initial_density
+# Use while Loop until the density exceeds 90%
+while current_density <= 90:
+    # Double the density each day
+    current_density *= 2
+    day += 1
+# Calculate the number of days required to exceed 90% density
+holiday_days = day - 1
+print(f"On day {holiday_days}, the cell density goes over 90%. This is the maximum number of days you can have a holiday from the lab.")
