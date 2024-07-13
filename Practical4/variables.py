b6eef63ee7219808a011733c514ee90b25fa5bcc
xiangjunle <junle.23@intl.zju.edu.cn>
2024-07-13 11:03:38 +0800

    again

---
+++ Practical4/variables.py
@@ -0,0 +1,11 @@
+# minutes to run 5 km
+a=40
+b=36
+c=30
+# The difference between different training methods
+d=a-b
+e=b-c
+if d>e:
+    print("d is bigger, means that running exercises had a greater improvement on running time")
+else:
+    print("e is bigger, means that strength exercises had a greater improvement on running time")
